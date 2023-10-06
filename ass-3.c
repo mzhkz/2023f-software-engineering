763ee896eb4b44e04b8b178d09f3563f6cd95f68
moz <moz@sfc.wide.ad.jp>
2023-10-06 20:29:33 +0900

    add: list sort

---
+++ ass-3.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef struct list {
+  int data;
+  struct list *next;
+} List;
+
+void print_list(List *s) {
+  while (1)
+  {
+    if (s == NULL) {
+      return;
+    }
+    printf("%d ", s->data);
+    s = s->next;
+  }
+   printf("\n");
+}
+
+
+List* combine(List *p, List *q) {
+  if (p == NULL)
+  {
+    return q;
+  }
+  else if (q == NULL)
+  {
+    return p;
+  }
+
+  List *s = p;
+
+  // リストの末尾を見つけるためにループを使う必要はありません
+  while (p->next != NULL) {
+    p = p->next;
+  }
+
+  p->next = q;
+  return s;
+}
+
+
+
+/**
+ * @brief 
+ * 
+ * @param s 
+ */
+List* sort(List *s){
+
+  if (s == NULL) {
+    return NULL;
+  }
+
+  List *p = NULL, *p_start = NULL;
+  List *q = NULL, *q_start = NULL;
+  List *t = s->next;
+
+  if (t == NULL)
+  {
+    return s;
+  }
+
+  while (1)
+  {
+    if (t == NULL) {
+      break;
+    }
+
+    List *next = t->next;
+    t->next = NULL;
+    if (s->data >= t->data)
+    {
+      if (q == NULL) {
+        q = t;
+        q_start = q;
+      }
+      else
+      {
+        q->next = t;
+        q = t;
+      }
+    } else {
+      if (p == NULL) {
+        p = t;
+        p_start = p;
+      } else {
+        p->next = t;
+        p = t;
+      }
+    }
+    t = next;
+  }
+  s->next = NULL;
+  return combine(combine(sort(q_start), s), sort(p_start));
+}
+
+int main(){
+  List *s, *p, *q;
+  p = malloc(sizeof(List));
+  s = p;
+  int size = 10;
+  for (int i = 1; i < size; i++)
+  {
+    q = malloc(sizeof(List));
+    p->data = size - i;
+    p -> next = q;
+    p = q;
+  }
+  q -> next = NULL;
+
+  printf("\n### original ###\n");
+  print_list(s);
+
+  printf("\n### sorted ###\n");
+  List *r = sort(s);
+  print_list(r);
+
+  return 0;
+}
+
+// int main(){
+//   List *s, *p, *q, *z;
+//   p = malloc(sizeof(List));
+//   q = malloc(sizeof(List));
+//   z = malloc(sizeof(List));
+
+//   q->data = 100;
+//   z->data = 200;
+//   p->next = z;
+
+//   s = combine(NULL, q);
+//   print_list(s);
+
+//   return 0;
+// }
